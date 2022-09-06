0aff82f366e07d178957ec4504ce67b5546b60ca
laoderidho <2081007@unai.edu>
2022-09-06 13:38:41 +0700

    update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Ridho!");
+using System; 
+
+namespace Basics {
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Hello Ridho");
+
+        }
+    }
+}
