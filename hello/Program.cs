81e72c1d120db945283575bcb22a524eb8e3790f
laoderidho <2081007@unai.edu>
2022-09-06 14:56:03 +0700

    update

--- hello/Program.cs
+++ hello/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Reflection;
+
+Assembly? assembly = Assembly.GetEntryAssembly();
+
+if(assembly==null) return;
+
+foreach(AssemblyName name in assembly.GetReferencedAssemblies){
+    Assembly a = Assembly.Load(name);
+    int methodCount = 0; 
+    foreach(TypeInfo t in a.DefinedTypes){
+        methodCount += t.GetMethods().Count();
+    }
+    Console.WriteLine("{0:NO} types with {1:NO} methofd in {2} assembly.", arg0: a.DefinedTypes.Count(), arg1: methodCount, arg2: name.Name);
+}
