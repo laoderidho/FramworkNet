ab22f189a7531f1d267140df779806b30b06efe0
laoderidho <2081007@unai.edu>
2022-09-19 20:50:38 +0700

    update

---
+++ 2081007_asigment1/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("size of sbyte"+sizeof(sbyte));
+Console.WriteLine("min value sbyte = "+ sbyte.MinValue);
+Console.WriteLine("max value sbyte=" + sbyte.MaxValue);
+
+Console.WriteLine("size of byte"+sizeof(byte));
+Console.WriteLine("min value byte=" +byte.MinValue);
+Console.WriteLine("max value byte-" + byte.MaxValue);
+
+Console.WriteLine("size of short"+sizeof(short));
+Console.WriteLine("min value short=" + short.MinValue);
+Console.WriteLine("max value short=" + short.MaxValue);
+
+Console.WriteLine("size of ushort"+sizeof(ushort));
+Console.WriteLine("min value ushort="+ ushort.MinValue);
+Console.WriteLine("max value ushort=" +ushort.MaxValue);
+
+Console.WriteLine("size of int"+sizeof(int));
+Console.WriteLine("min value int=" + int.MinValue);
+Console.WriteLine("max value int=" + int.MaxValue);
+
+Console.WriteLine("size of uint"+sizeof(uint));
+Console.WriteLine("min value uint=" + uint.MinValue);
+Console.WriteLine("max value uint=" +uint.MaxValue);
+
+Console.WriteLine("size of long"+sizeof(long));
+Console.WriteLine("min value long=" + long.MinValue);
+Console.WriteLine("max value long=" +long.MaxValue);
+
+Console.WriteLine("size of ulong"+sizeof(ulong));
+Console.WriteLine("min value ulong=" + ulong.MinValue);
+Console.WriteLine("max value ulong=" +ulong.MaxValue);
+
+Console.WriteLine("size of float"+sizeof(float));
+Console.WriteLine("min value float=" + float.MinValue);
+Console.WriteLine("max value float=" +float.MaxValue);
+
+Console.WriteLine("size of double"+sizeof(double));
+Console.WriteLine("min value double=" + double.MinValue);
+Console.WriteLine("max value double=" +double.MaxValue);
+
+Console.WriteLine("size of decimal"+sizeof(decimal));
+Console.WriteLine("min value decimal=" + decimal.MinValue);
+Console.WriteLine("max value decimal=" +decimal.MaxValue);
