ce93164ad9d11d14325602198e5e951a8a4e2b15
laoderidho <2081007@unai.edu>
2022-10-25 15:18:53 +0700

    update

---
+++ pactLibraryModern/Program.cs
@@ -0,0 +1,10 @@
+﻿class Person
+{
+  private string name; // field
+
+  public string Name   // property
+  {
+    get { return name; }   // get method
+    set { name = "Ridho"; }  // set method
+  }
+}
