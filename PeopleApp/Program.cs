7f36d1bfb3e2a488d87c6fc9670e461e35fb7ad4
laoderidho <2081007@unai.edu>
2022-10-25 21:30:43 +0700

    udpate

--- PeopleApp/Program.cs
+++ PeopleApp/Program.cs
@@ -1,12 +1,18 @@
-﻿using paackt.Shared;
+﻿using Packt.Shared;
 using static System.Console;
 
-
-person ms4 = new(){
-    nama = "Ridho",
-    Hometown = "Bandung",
-    tanggalLahir = new(2002, 6, 5)
-};
-
-WriteLine(ms4.Asal);
-Write($"Sekarang Berumur{ms4.usia}");
+Person harry = new() { Name = "Harry" };
+Person mary = new() { Name = "Mary" };
+Person jill = new() { Name = "Jill" };
+// call instance method
+Person baby1 = mary.ProcreateWith(harry);
+baby1.Name = "Gary";
+// call static method
+Person baby2 = Person.Procreate(harry, jill);
+WriteLine($"{harry.Name} has {harry.Children.Count} children.");
+WriteLine($"{mary.Name} has {mary.Children.Count} children.");
+WriteLine($"{jill.Name} has {jill.Children.Count} children.");
+WriteLine(
+format: "{0}'s first child is named \"{1}\".",
+arg0: harry.Name,
+arg1: harry.Children[0].Name);
