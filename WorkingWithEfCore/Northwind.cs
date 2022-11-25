80563af62a4cd69e9a40df006b5b34b1ac3f7a41
laoderidho <2081007@unai.edu>
2022-11-25 21:18:42 +0700

    update 2 day ago

--- WorkingWithEfCore/Northwind.cs
+++ WorkingWithEfCore/Northwind.cs
@@ -4,14 +4,19 @@ namespace Packt.Shared;
 // this manages the connection to the database
 public class Northwind : DbContext
 {
+    // these properties map to tables in the database
+    public DbSet<Category>? Categories { get; set; }
+    public DbSet<Product>? Products { get; set; }
     protected override void OnConfiguring(
-    DbContextOptionsBuilder optionsBuilder)
+        DbContextOptionsBuilder optionsBuilder)
     {
         if (ProjectConstants.DatabaseProvider == "SQLite")
         {
             string path = Path.Combine(
             Environment.CurrentDirectory, "Northwind.db");
+
             WriteLine($"Using {path} database file.");
+
             optionsBuilder.UseSqlite($"Filename={path}");
         }
         else
@@ -21,6 +26,23 @@ public class Northwind : DbContext
             "Integrated Security=true;" +
             "MultipleActiveResultSets=true;";
             optionsBuilder.UseSqlServer(connection);
+        }
+
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // example of using Fluent API instead of attributes
+        // to limit the length of a category name to 15
+        modelBuilder.Entity<Category>()
+        .Property(category => category.CategoryName)
+        .IsRequired() // NOT NULL
+        .HasMaxLength(15);
+        if (ProjectConstants.DatabaseProvider == "SQLite")
+        {
+            //added to "fix" the lack of decimal support in SQLite
+            modelBuilder.Entity<Product>()
+            .Property(product => product.Cost)
+            .HasConversion<double>();
         }
     }
 }
